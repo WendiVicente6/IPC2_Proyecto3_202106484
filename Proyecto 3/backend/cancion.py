9eda696d17c7076a1d1c416f3d76cf2b0ee4fb2a
WendiVicente6 <3005923960101@ingenieria.usac.edu.gt>
2022-11-02 23:44:49 -0600

    Docuemtnacion

--- Proyecto 3/backend/cancion.py
+++
@@ -1,9 +0,0 @@
-class Cancion:
-    def __init__(self,name,artist,image,album):
-        self.name=name
-        self.artist=artist
-        self.image=image
-        self.album=album
-    
-    def getCancion(self):
-        return self
--- Proyecto 3/backend/gestor.py
+++ Proyecto 3/backend/gestor.py
@@ -1,5 +1,5 @@
 from usuario import Usuario
-from cancion import Cancion
+
 from Clientes import Clientes
 from Categorias import Categorias
 from Configuraciones import Configuraciones
@@ -16,8 +16,7 @@ class Gestor:
         self.Categorias=[]
         self.Configuracion=[]
         self.Instancia=[]
-        self.usuarios.append(Usuario('Jacky','Benitez','admin','admin'))
-        self.usuarios.append(Usuario('Kirby','SuperStar','kirby','kirby'))
+
     
     def obtener_usuarios(self):
         return json.dumps([ob.__dict__ for ob in self.usuarios])
